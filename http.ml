159af6f131cace7a7ea3b66037d677e3b1192d9d
hellojukay <licong@qianxin.com>
2021-11-30 17:48:55 +0800

    update

--- http.ml
+++ http.ml
@@ -1,7 +1,25 @@
 class http tcp =
-  object
+  object (self)
     method parse_http () =
       Log.info (Printf.sprintf "[%d] get message" (Unix.getpid ()));
-      let first_line = input_line (Unix.in_channel_of_descr tcp) in
-      Log.info (Printf.sprintf "%s\n" first_line)
+      let first_line = self#first_line () in
+      Log.info (Printf.sprintf "%s\n" first_line);
+      self#write "Hello World";
+      Unix.close tcp
+
+    method first_line () =
+      let fd = Unix.in_channel_of_descr tcp in
+      let line = input_line fd in
+      try
+        while true do
+          let s = input_line fd in
+          if Str.string_match (Str.regexp "\r?\n?") s 0 then raise End_of_file
+          else s |> Log.info
+        done;
+        line
+      with End_of_file -> line
+
+    method write msg =
+      let fd = Unix.out_channel_of_descr tcp in
+      Printf.fprintf fd "%s" msg
   end
